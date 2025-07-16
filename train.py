061412e1ccda89239fe4d753ffafe01d66b03167
Dhia Naouali <dhia12naouali@gmail.com>
2025-07-16 01:44:35 +0100

    trainer

---
+++ train.py
@@ -0,0 +1,17 @@
+
+
+
+class Trainer:
+    def __init__(self, config):
+        # seed
+        # init models
+        # print param counts
+        # setup optimizers
+        # setup loss
+        # setup loader
+        # ADA ?
+        # scalers
+        # checkpoint manager
+        # compile ?
+        # metrics
+        # fixed noise sample
