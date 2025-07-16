fee318854835a581758e26f07652e720607c23cb
Dhia Naouali <dhia12naouali@gmail.com>
2025-07-16 01:08:41 +0100

    init utils

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import random
+import numpy as np
+import torch
+
+
+
+def seed_all(seed=12):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False # hoping to land on good enough kernels T-T
+
+
+def count_params(model, trainable_only=False):
+    if trainable_only:
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return sum(p.numel() for p in model.parameters()) 
+
+
+def log_to_wandb():
+    ...
