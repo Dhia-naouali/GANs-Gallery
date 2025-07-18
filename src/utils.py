50c401e135300017096426406eb1942430911569
Dhia Naouali <dhia12naouali@gmail.com>
2025-07-18 13:55:40 +0100

    rebased step to parent class + added warmup cosine annealing

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 import wandb # I should get a wandb sticker
-import inspect
+import math
 
 import torch
 from torch import optim
@@ -73,6 +73,10 @@ class Scheduler(optim.lr_scheduler._LRScheduler):
         ):
             return False
         return True
+    
+    def step(self, epoch_call=False):
+        if self.take_step(epoch_call):
+            super().step()
 
 
 
@@ -84,10 +88,6 @@ class WarmUpLinearDecay(Scheduler):
         self.min_lr = config.get("min_lr", 5e-8)
         super().__init__(optimizer)
 
-    def step(self, epoch_call=False):
-        if super().take_step(epoch_call):
-            super().step()
-
 
     def get_lr(self):
         progress = self.last_epoch / self.total_steps
@@ -95,6 +95,34 @@ class WarmUpLinearDecay(Scheduler):
         
         lrs = [
             base_lr * (1 - progress) + self.min_lr * progress
+            for base_lr in self.base_lrs
+        ]
+        return lrs
+
+
+class WarmUpCosine(Scheduler):
+    def __init__(self, optimizer, total_steps, config):
+        self.total_steps = total_steps
+
+        self.eta_min_ratio = config.get("eta_min_ratio", 1e-2)
+        warmup_phase = config.get("warm_up_phase", 0.05)
+        if warmup_phase > 1:
+            warmup_phase /= 100
+
+        self.warmup_steps = int(total_steps * warmup_phase)
+        self.peak_lr = optimizer.param_groups[0]['lr']
+        super().__init__(optimizer)
+
+
+    def get_lr(self):
+        if self.last_epoch < self.warmup_steps:
+            scale = self.last_epoch / max(1, self.warmup_steps)
+        else:
+            progress = (self.last_epoch - self.warmup_steps) / max(1, self.total_steps - self.warmup_steps)
+            scale = self.eta_min_ratio + (1 - self.eta_min_ratio) * 0.5 * (1 + math.cos(math.pi * progress))
+
+        lrs = [
+            base_lr * scale
             for base_lr in self.base_lrs
         ]
         return lrs
