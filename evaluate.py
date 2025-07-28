eab5c56eee996c236c056399bef5439b2fd391e1
Dhia Naouali <dhia12naouali@gmail.com>
2025-07-28 23:06:33 +0100

    evaluation script, mid way there

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,11 @@
+# pip install torchmetrics[image]
 import torch
-from torchmetrics import (...)
+from torchmetrics.image import (
+    FrechetInceptionDistance,
+    InceptionScore,
+    LearnedPerceptualImagePatchSimilarity,
+    KernelInceptionDistance
+)
 from tqdm import tqdm
 
 
@@ -9,12 +15,12 @@ class Evaluator:
         self.config = config
         self.device = device
         
-        self.FID = ...
-        self.IS = ...
-        self.KID = ...
-        self.LPIPS = ...
-        
-        
+        self.FID = FrechetInceptionDistance(normalize=True).to(device)
+        self.IS = InceptionScore(normalize=True).to(device)
+        self.KID = KernelInceptionDistance(normalize=True).to(device)
+        self.LPIPS = LearnedPerceptualImagePatchSimilarity(normalize=True).to(device)
+
+
     @torch.no_grad
     def generate_samples(self, batch_size, num_batches):
         for _ in num_batches:            
@@ -24,39 +30,53 @@ class Evaluator:
                 device=self.device
             )
             
-            samples = self.G(noise).byte()
-            yield samples
+            samples = self.G(noise)            
+            yield (samples * .5 + .5).byte()
     
     def load_samples(self, dataloader, batch_size, num_batches):
 
         for _ in tqdm(num_batches):
             # to make sure that the batch size is the same used in the loader
             samples = next(dataloader)[0]["image"] # to check if compatible with dali
-            yield samples
+            yield (samples * .5 + .5).byte()
             
             
     def compute_fid(self, fake_images, real_images):
-        ...
+        self.FID.reset()
+        self.FID.update(fake_images, real=False)
+        self.FID.update(real_images, real=True)
+        fid_mean, fid_std = self.FID.compute().item()
+        return fid_mean.item(), fid_std.item()
+
         
-        
-    def compute_inception_score(self, fake_images, rela_images):
-        ...
+    def compute_inception_score(self, fake_images):
+        self.IS.reset()
+        self.IS.update(fake_images)
+        return self.IS.compute()
         
     
     def compute_kid(self, fake_images, real_images):
-        ...
+        self.KID.reset()
+        self.KID.update(fake_images, real=False)
+        self.KID.update(real_images, real=True)
+        kid_mean, kid_std =  self.KID.compute()
+        return kid_mean.item(), kid_std.item()
         
         
-    def compute_lpips_diversity(self, fake_images, ...):
-        ...
+    @torch.no_grad        
+    def compute_lpips_diversity(self, fake_images, real_images):
+        return self.LPIPS(fake_images, real_images)
     
     def evaluta(self, dataloader, batch_size, num_batches):
         fake_images = self.generate_samples()
         real_images = self.load_samples()
-        fid = self.compute_fid()
-        inception_score = self.compute_inception_score()
-        lpips = self.compute_lpips_diversity()
-        kid = self.compute_kit()
+
+
+        for humm in num_batches:
+            fid = self.compute_fid(fake_images, real_images)
+            inception_score = self.compute_inception_score(fake_images)
+            lpips = self.compute_lpips_diversity(fake_images, real_images)
+            kid = self.compute_kit(fake_images, real_images)
         
         return {
             "FID": ,
